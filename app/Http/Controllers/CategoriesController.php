35e9d7c589eefcac8a3894a98049d67f275dee30
yamato <yunantobiantoro8@gmail.com>
2024-08-06 14:50:41 +0700

    Tags dan Categories controller

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Categories;
+use Illuminate\Support\Facades\Validator; 
+
+class CategoriesController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:50', 
+            'slug' => 'required|string|max:50|unique:posts', 
+
+            // Add more validation rules as needed
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $kategori = Categories::create($request->all());
+        return response()->json(['message' => 'Kategori berhasil dibuat', 'kategori' => $kategori], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $kategori = Categories::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:50',
+            'slug' => 'required|string|max:50|unique:posts,slug,' . $kategori->id,  
+
+            // Add more validation rules as needed
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $kategori->update($request->all());
+        return response()->json(['message' => 'Data berhasil diperbarui', 'data' => $kategori]);
+
+    }
+}
---
+++ app/Http/Controllers/TagsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tags;
+use Illuminate\Support\Facades\Validator; 
+
+
+class TagsController extends Controller
+{
+    //
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:50', 
+            'slug' => 'required|string|max:50|unique:posts', 
+
+            // Add more validation rules as needed
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $tags = Tags::create($request->all());
+        return response()->json(['message' => 'Kategori berhasil dibuat', 'kategori' => $tags], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $tags = Tags::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:50',
+            'slug' => 'required|string|max:50|unique:posts,slug,' . $tags->id,  
+
+            // Add more validation rules as needed
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $tags->update($request->all());
+        return response()->json(['message' => 'Data berhasil diperbarui', 'data' => $tags]);
+
+    }
+}
